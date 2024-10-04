47abb66fde6156bcb1a5cd2f8d4a5f67a120115c
Vinícius Regazio <viniciusregazio@icloud.com>
2024-10-04 17:22:09 -0300

    Atividade-07.kt

---
+++ ViniciusRegazio/Atividade-07.kt
@@ -0,0 +1,17 @@
+import kotlin.math.*
+fun main() {
+  
+    println(max(5,10))
+    println(min(5,10))
+    println(sqrt(49f))
+    
+    println(PI)
+    println(E)
+
+    println(round(35465.4000009987898789766))
+    println(round(PI))
+    println(round(E))
+
+    val PI = 3.14159
+    print(PI)
+}
